110f0d4b3ea7ded3a2348ae129752c3bc2bca783
Fabian Holler <94364725+Tanzkalmar35@users.noreply.github.com>
2024-12-04 20:33:09 +0100

    Taking another approach

--- lua/cnippets/init.lua
+++ lua/cnippets/init.lua
@@ -1,44 +1,26 @@
-local function check_for_valid_ts_field(line)
-	-- if line == nil or line == "" then
-	-- 	print("Lines can not be empty.")
-	-- 	return false
-	-- end
-	--
-	-- if not line:match("^(%s*(public|private|protected)?)%s*(readonly%s+)?(%w+)%s*:%s*([%w%s]+)%s*;?") then
-	-- 	print("At least 1 selected line is no valid ts field.")
-	-- 	return false
-	-- end
+local function extractFieldNameAndType(line)
+	local fieldName
+	local fieldType
+	local lastWord = ""
 
-	for w in string.gmatch(line, "%a+") do
-		print(w)
+	for word in string.gmatch(line, "%a+") do
+		print(word)
+		if string.match(word, "%a:") then
+			fieldName = word:sub(0, word:len())
+		elseif word == ":" then
+			fieldName = lastWord
+		end
+		if lastWord == ":" then
+			fieldType = word
+		end
+		lastWord = word
 	end
 
-	-- if wordInLine. then
-	-- 	print("Visibility modifier found: " .. visibility)
-	-- else
-	-- 	print("No visibility modifier found.")
-	-- end
-	--
-	-- -- Step 2: Check for field name
-	-- local field_name = string.match(line, "readonly")
-	-- if field_name then
-	-- 	print("Field name found: " .. field_name)
-	-- else
-	-- 	print("No field name found.")
-	-- end
-	--
-	-- -- Step 3: Check for type annotation
-	-- local type_annotation = line:match(":%s*([%w%s]+)")
-	-- if type_annotation then
-	-- 	print("Type annotation found: " .. type_annotation)
-	-- else
-	-- 	print("No type annotation found.")
-	-- end
-
-	return true
+	print(fieldName)
+	print(fieldType)
 end
 
-local function print_selected_lines()
+local function printSelectedLines()
 	print("Calling function!")
 	local s_start = vim.fn.getpos("'<")
 	local s_end = vim.fn.getpos("'>")
@@ -46,11 +28,7 @@ local function print_selected_lines()
 	local lines = vim.api.nvim_buf_get_lines(0, s_start[2] - 1, s_end[2], false)
 
 	for _, line in ipairs(lines) do
-		if check_for_valid_ts_field(line) then
-			print(line)
-		else
-			print("Something strange happened...")
-		end
+		extractFieldNameAndType(line)
 	end
 end
 
@@ -59,5 +37,5 @@ end
 -- end, {})
 
 return {
-	print_selected_lines = print_selected_lines,
+	printSelectedLines = printSelectedLines,
 }
